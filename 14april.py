ae1bf490fc10c6b5c54aa388be9a792d1e170f79
redmachin3 <cyberguru14@gmail.com>
2022-04-14 12:30:18 +0300

    release v1

---
+++ 14april.py
@@ -0,0 +1,10 @@
+import sys
+
+var1 = "Max is too lazy programmer"
+var2 = 1234567890
+var3 = True
+print(sys.getsizeof(var1))
+print(sys.getsizeof(var2))
+print(sys.getsizeof(var3))
+
+"""Таким образом я могу узнать размер расходуемой памяти переменной(ыми)"""
